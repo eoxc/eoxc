44ca3205c869d798ae1698560a272449842b6c41
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2016-05-06 19:29:54 +0200

    Fixing webpack config for TimeSliderView

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,17 @@
+var webpack = require("webpack");
+var path = require("path");
 
 module.exports = {
     entry: {
         openlayers: './test/apps/OpenLayers/main.js',
         //cesium: './test/apps/Cesium/main.js',
+    },
+    resolve: {
+        modulesDirectories: ["web_modules", "node_modules", "bower_components"],
+        alias: {
+            // necessary to avoid multiple packings of backbone due to marionette
+            backbone: path.join(__dirname, 'node_modules', 'backbone', 'backbone')
+        },
     },
     output: {
         path: "./dist",
@@ -13,7 +22,18 @@ module.exports = {
     module: {
         loaders: [
             {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"},
-            {test: /\.json$/, exclude: /node_modules/, loader: 'json-loader'}
+            {test: /\.json$/, exclude: /node_modules/, loader: 'json-loader'},
+            //{test: /\.css$/, loaders: 'style-loader!css-loader'},
+            {test: /\.css$/, loaders: ['style', 'css']},
         ]
     },
+    plugins: [
+        new webpack.ResolverPlugin(
+            new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin(".bower.json", ["main"])
+        ),
+        /*new webpack.ProvidePlugin({
+            d3: "d3",
+            "window.d3": "d3"
+        })*/
+    ]
 };
