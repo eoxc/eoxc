8890147223fcb6abe898a0771ded41cf691d6041
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2016-05-12 20:13:48 +0200

    Allowing intrinsic sorting in LayersCollection

--- src/core/models/LayersCollection.js
+++ src/core/models/LayersCollection.js
@@ -5,13 +5,25 @@ import Backbone from 'backbone';
  * @memberof core/models
  */
 
-class LayersCollection extends Backbone.Collection {
-  getLayerById(id) {
-    return this.find(layer => (layer.get('identifier') === id));
-  }
-}
+const LayersCollection = Backbone.Collection.extend({
+  model: LayerModel,
+  comparator: 'ordinal',
 
-LayersCollection.prototype.model = LayerModel;
-
+  /**
+   * Add a single model or an array of models to the collection. Automtically
+   * sets the 'ordinal' property for sorting.
+   */
+  add(layerDefinition, options) {
+    if (Array.isArray(layerDefinition)) {
+      for (let i = 0; i < layerDefinition.length; ++i) {
+        layerDefinition[i].ordinal = this.size() + i;
+      }
+    }
+    else {
+      layerDefinition.ordinal = this.size();
+    }
+    return Backbone.Collection.prototype.add.call(this, layerDefinition, options);
+  },
+});
 
 export default LayersCollection;
