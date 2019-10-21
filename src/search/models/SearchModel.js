eoxc/eoxc

commit c914b5dc9aebd32628e82e6c4fe4a22656c9de2f

disable load-more querying of opensearch first page before all other (#21)
- disable load-more querying of opensearch first page before all other
- bump opensearch-browser to v2.0.0-alpha.3 to enable it, url-parse to v1.4.7

src/search/models/SearchModel.js
@@ -85,16 +85,17 @@ class SearchModel extends Backbone.Model {
     const layerModel = this.get('layerModel');
     const filtersModel = this.get('filtersModel');
     const mapModel = this.get('mapModel');
-
-    this.doSearchDebounced(layerModel, filtersModel, mapModel, false, this.get('hasLoaded'));
+    // if loading more, total results information can be used
+    this.doSearchDebounced(layerModel, filtersModel, mapModel, false, this.get('hasLoaded'), this.get('totalResults'));
   }
 
-  doSearch(layerModel, filtersModel, mapModel, reset = true, startIndex = 0) {
+  doSearch(layerModel, filtersModel, mapModel, reset = true, startIndex = 0, totalResults = undefined) {
     this.cancelSearch();
     const searchOptions = {
-      itemsPerPage: this.get('defaultPageSize'),
+      itemsPerPage: this.get('itemsPerPage') || this.get('defaultPageSize'),
       maxCount: (startIndex === 0) ? this.get('maxCount') : this.get('loadMore'),
       startIndex,
+      totalResults,
     };
     const request = searchAllRecords(layerModel, filtersModel, mapModel, searchOptions);
     this.prevRequest = request;

src/search/opensearch/OpenSearchWorker.js
@@ -33,6 +33,8 @@ function getService(url, description) {
 
 function searchAll(url, method, filterParams, mapParams, options, format, description, maxUrlLength, dropEmptyParameters, parseOptions) {
   const maxCount = options.maxCount;
+  const totalResults = options.totalResults;
+  const itemsPerPage = options.itemsPerPage;
 
   return getService(url, description)
     .then((service) => {
@@ -44,6 +46,8 @@ function searchAll(url, method, filterParams, mapParams, options, format, descri
         maxUrlLength,
         dropEmptyParameters,
         parseOptions,
+        totalResults,
+        serverItemsPerPage: itemsPerPage,
       });
       return paginator.searchFirstRecords(maxCount);
     });

commit 5ca25374957828bc5de8332edf85ad1ccd05ab14

fix search on mapMove using previous itemsPerPage regression bug from  c914b5dc9aebd32628e82e6c4fe4a22656c9de2f

src/search/models/SearchModel.js
@@ -92,7 +92,7 @@ class SearchModel extends Backbone.Model {
   doSearch(layerModel, filtersModel, mapModel, reset = true, startIndex = 0, totalResults = undefined) {
     this.cancelSearch();
     const searchOptions = {
-      itemsPerPage: this.get('itemsPerPage') || this.get('defaultPageSize'),
+      itemsPerPage: this.get('defaultPageSize'),
       maxCount: (startIndex === 0) ? this.get('maxCount') : this.get('loadMore'),
       startIndex,
       totalResults,