f1131c53750c519f693a2b5ee7b7fffcb3ab62f3
Lubomir Dolezal <lubomir.bucek@eox.at>
2022-09-16 13:58:29 +0200

    wip: test concurrency
    currently due to preliminary dequeue does not fetch any other than first x

--- src/core/views/RecordItemView.js
+++ src/core/views/RecordItemView.js
@@ -2,8 +2,8 @@ import Marionette from 'backbone.marionette';
 
 import template from './RecordItemView.hbs';
 import './RecordItemView.css';
-
 import imageError from './RecordItemViewImageError.hbs';
+import { isValidUrl } from '../util';
 
 const RecordItemView = Marionette.ItemView.extend(/** @lends core/views/layers.RecordItemView# */{
   template,
@@ -21,49 +21,104 @@ const RecordItemView = Marionette.ItemView.extend(/** @lends core/views/layers.R
     this.collection = this.model.collection || options.collection;
     this.thumbnailUrlPattern = options.thumbnailUrlPattern;
     this.fallbackThumbnailUrl = options.fallbackThumbnailUrl;
+    this.usedUrl = null;
   },
 
   templateHelpers() {
     const time = this.model.get('properties').time;
     const start = Array.isArray(time) ? time[0] : time;
-    let thumbnailUrl = this.model.getThumbnailUrl(
-      this.collection && this.collection.searchModel ? this.collection.searchModel.get('layerModel').get('search.thumbnailUrlTemplate')
-                      : undefined
-    );
-    if (this.thumbnailUrlPattern && !(new RegExp(this.thumbnailUrlPattern)).test(thumbnailUrl)) {
-      thumbnailUrl = '';
-    }
     return {
-      thumbnailUrl,
       date: start.toISOString().substring(0, 10),
       time: start.toISOString().substring(11, 19),
     };
   },
 
-  onRender() {
+  setImageSrc(imageObjectURL) {
     const $img = this.$('img');
-    $img
-      .one('load', () => this.$('img').fadeIn('slow'))
-      .one('error', () => {
-        const quickLookUrl = this.model.getQuickLookUrl(
-          this.collection && this.collection.searchModel ? this.collection.searchModel.get('layerModel').get('search.quickLookUrlTemplate')
-                          : undefined
-        );
-        if (quickLookUrl) {
-          $img
-          .attr('src', quickLookUrl)
-          .one('load', () => this.$('img').fadeIn('slow'))
-          .one('error', () => {
-          });
-        } else if (this.fallbackThumbnailUrl) {
-          $img
-            .one('error', () => $img.attr('alt', imageError()))
-            .attr('src', this.fallbackThumbnailUrl)
-            .addClass('error');
+    $img.attr('src', imageObjectURL);
+  },
+
+  boundHandler(imageBlob) {
+    this.setImageSrc(imageBlob);
+    this.unSetupQueueListener();
+  },
+
+  setupQueueListener() {
+    const id = this.getID();
+    window.BackboneEventBus.on(`fetch:${id}`, this.boundHandler, this);
+  },
+
+  unSetupQueueListener() {
+    const id = this.getID();
+    window.BackboneEventBus.off(`fetch:${id}`, null, this);
+  },
+
+  getID() {
+    return `${this.usedUrl}`;
+  },
+
+  enqueueThumbnailImageFetch() {
+    let url = this.model.getThumbnailUrl(
+      this.collection && this.collection.searchModel ? this.collection.searchModel.get('layerModel').get('search.thumbnailUrlTemplate')
+        : undefined
+    );
+    if (this.thumbnailUrlPattern && !(new RegExp(this.thumbnailUrlPattern)).test(url)) {
+      url = '';
+    }
+    if (isValidUrl(url)) {
+      if (window.requestQueue) {
+        const urlObj = new URL(url);
+        if (window.BackboneEventBus.cache[urlObj.href]) {
+          const imageBlobUrl = window.BackboneEventBus.cache[urlObj.href];
+          this.setImageSrc(imageBlobUrl);
         } else {
-          $img.attr('alt', imageError());
+          this.usedUrl = urlObj.href;
+          this.setupQueueListener();
+          const id = this.getID();
+          if (!window.requestQueue.has(id)) {
+            window.requestQueue.enqueue(urlObj, {}, {
+              'lifo': true, 'itemID': this.getID(),
+            });
+          }
         }
-      });
+      } else {
+        fetch(url)
+          .then((response) => response.blob())
+          .then((imageBlob) => {
+            // Then create a local URL for that image
+            const imageObjectURL = URL.createObjectURL(imageBlob);
+            this.setImageSrc(imageObjectURL);
+          }).bind(this);
+      }
+
+    }
+  },
+
+  onRender() {
+    this.enqueueThumbnailImageFetch();
+    // const $img = this.$('img');
+    // $img
+    //   .one('load', () => this.$('img').fadeIn('slow'))
+    //   .one('error', () => {
+    //     const quickLookUrl = this.model.getQuickLookUrl(
+    //       this.collection && this.collection.searchModel ? this.collection.searchModel.get('layerModel').get('search.quickLookUrlTemplate')
+    //                       : undefined
+    //     );
+    //     if (quickLookUrl) {
+    //       $img
+    //       .attr('src', quickLookUrl)
+    //       .one('load', () => this.$('img').fadeIn('slow'))
+    //       .one('error', () => {
+    //       });
+    //     } else if (this.fallbackThumbnailUrl) {
+    //       $img
+    //         .one('error', () => $img.attr('alt', imageError()))
+    //         .attr('src', this.fallbackThumbnailUrl)
+    //         .addClass('error');
+    //     } else {
+    //       $img.attr('alt', imageError());
+    //     }
+    //   });
   },
 
   onAttach() {
@@ -93,6 +148,21 @@ const RecordItemView = Marionette.ItemView.extend(/** @lends core/views/layers.R
 
   onItemMouseOut() {
     this.highlightModel.unHighlight(this.model.attributes);
+  },
+
+  onDestroy() {
+    if (window.requestQueue) {
+      this.unSetupQueueListener(this.usedUrl);
+      window.requestQueue.dequeue(this.usedUrl);
+    }
+  },
+
+  onDestroy() {
+    if (window.requestQueue) {
+      this.unSetupQueueListener();
+    }
+    const id = this.getID();
+    window.requestQueue.dequeue(id);
   },
 });
 
